ed8d3851c8114bb8206d68badd5f194f7f0d62aa
nite <noppana08@gmail.com>
2024-09-09 21:59:45 +0700

    Stack is valid Expression

---
+++ StackCheckExpression.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class StackCheckExpression {
+
+    public static boolean isValidExpression(String s){
+        Stack <Character> stack = new Stack<>();
+
+        for(Character c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c ==  ']'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(c == ')' && top != '(' ||
+                    c == '}' && top != '{' ||
+                    c == ']' && c != '[' ){
+                    return false;
+                }
+            } 
+            
+        }
+
+
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String input = "((1+2)*(3+4)/2))";
+        if(isValidExpression(input)){
+            System.out.println("corect parentheses ");
+        }else {
+            System.out.println("Not corect parentheses");
+        }
+    }
+}
